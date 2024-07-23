2adf1bbf6e195c99180f1b66a6e5b00abdbed9b4
zaengetsu <106625404+zaengetsu@users.noreply.github.com>
2024-07-23 17:03:24 +0200

    ajout des route pour le paiment reussi et pour le paiment échoué

--- FrontEnd/src/router/router.ts
+++ FrontEnd/src/router/router.ts
@@ -1,113 +1,114 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import { useAuthStore } from '@/stores/user';
+import { createRouter, createWebHistory } from 'vue-router'
+import { useAuthStore } from '@/stores/user'
 
-import Register from '../pages/Register.vue';
-import Login from '../pages/Login.vue';
-import Home from "@/pages/Home.vue";
-import Products from "@/pages/Products.vue";
-import ForgotPassword from "@/pages/ForgotPassword.vue";
-import ConfirmEmail from "@/pages/ConfirmEmail.vue";
-import ResetPassword from '@/pages/resetPassword.vue';
-import AdminDashboard from "@/pages/admin_dashboard.vue";
-import ProductDetails from "@/pages/ProductDetails.vue";
-import Cart from "@/pages/Cart.vue";
-import NotFound from "@/pages/NotFound.vue";
-import ProfilPage from "@/pages/profil/profilPage.vue";
-import SearchResult from "@/components/common/SearchResult.vue";
-import Checkout from "@/pages/Checkout.vue";
-import Payment from "@/pages/Payment.vue";
-import PaymentCommande from "@/pages/PaymentCommande.vue";
-
+import Register from '../pages/Register.vue'
+import Login from '../pages/Login.vue'
+import Home from '@/pages/Home.vue'
+import Products from '@/pages/Products.vue'
+import ForgotPassword from '@/pages/ForgotPassword.vue'
+import ConfirmEmail from '@/pages/ConfirmEmail.vue'
+import ResetPassword from '@/pages/resetPassword.vue'
+import AdminDashboard from '@/pages/admin_dashboard.vue'
+import ProductDetails from '@/pages/ProductDetails.vue'
+import Cart from '@/pages/Cart.vue'
+import NotFound from '@/pages/NotFound.vue'
+import ProfilPage from '@/pages/profil/profilPage.vue'
+import SearchResult from '@/components/common/SearchResult.vue'
+import Checkout from '@/pages/Checkout.vue'
+import Payment from '@/pages/Payment.vue'
+import PaymentCommande from '@/pages/PaymentCommande.vue'
+import PaymentSuccess from '@/pages/PaymentSuccess.vue'
+import PaymentCancel from '@/pages/PaymentCancel.vue'
 
 const routes = [
-    {
-        path: '/register',
-        name: 'Register',
-        component: Register,
-        meta: { requiresAuth: false }
-    },
-    {
-        path: '/login',
-        name: 'Login',
-        component: Login,
-        meta: { requiresAuth: false }
-    },
-    {
-        path: '/home',
-        name: 'Home',
-        component: Home,
-        meta: { requiresAuth: false }
-    },
-    {
-        path: '/products',
-        name: 'Products',
-        component: Products,
-        meta: { requiresAuth: false }
-    },
-    {
-        path: '/forgot-password',
-        name: 'ForgotPassword',
-        component: ForgotPassword,
-        meta: { requiresAuth: false }
-    },
-    {
-        path: '/confirm-email/:token',
-        name: 'ConfirmEmail',
-        component: ConfirmEmail,
-        meta: { requiresAuth: false }
-    },
-    {
-        path: '/',
-        redirect: '/home',
-        meta: { requiresAuth: false }
-    },
-    {
-        path: '/reset-password/:resetToken',
-        name: 'ResetPassword',
-        component: ResetPassword,
-        meta: { requiresAuth: false }
-    },
-    {
-        path: '/:catchAll(.*)',
-        name: 'NotFound',
-        component: NotFound,
-        meta: { requiresAuth: false }
-    },
-    {
-        path: '/admin/dashboard',
-        name: 'AdminDashboard',
-        component: AdminDashboard,
-        meta: { requiresAuth: true, requiresAdmin: true },
-    },
-    {
-        path: '/product/:id',
-        name: 'ProductDetails',
-        component: ProductDetails,
-        meta: { requiresAuth: false }
-    },
-    {
-        path: '/cart',
-        name: 'Cart',
-        component: Cart,
-        meta: { requiresAuth: false }
-    },
-    {
-        path: '/profile',
-        name: 'ProfilPage',
-        component: ProfilPage,
-        meta: { requiresAuth: true }
-    },
-    {
-        path: '/search',
-        name: 'SearchResults',
-        component: SearchResult,
-    },
+  {
+    path: '/register',
+    name: 'Register',
+    component: Register,
+    meta: { requiresAuth: false }
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login,
+    meta: { requiresAuth: false }
+  },
+  {
+    path: '/home',
+    name: 'Home',
+    component: Home,
+    meta: { requiresAuth: false }
+  },
+  {
+    path: '/products',
+    name: 'Products',
+    component: Products,
+    meta: { requiresAuth: false }
+  },
+  {
+    path: '/forgot-password',
+    name: 'ForgotPassword',
+    component: ForgotPassword,
+    meta: { requiresAuth: false }
+  },
+  {
+    path: '/confirm-email/:token',
+    name: 'ConfirmEmail',
+    component: ConfirmEmail,
+    meta: { requiresAuth: false }
+  },
+  {
+    path: '/',
+    redirect: '/home',
+    meta: { requiresAuth: false }
+  },
+  {
+    path: '/reset-password/:resetToken',
+    name: 'ResetPassword',
+    component: ResetPassword,
+    meta: { requiresAuth: false }
+  },
+  {
+    path: '/:catchAll(.*)',
+    name: 'NotFound',
+    component: NotFound,
+    meta: { requiresAuth: false }
+  },
+  {
+    path: '/admin/dashboard',
+    name: 'AdminDashboard',
+    component: AdminDashboard,
+    meta: { requiresAuth: true, requiresAdmin: true }
+  },
+  {
+    path: '/product/:id',
+    name: 'ProductDetails',
+    component: ProductDetails,
+    meta: { requiresAuth: false }
+  },
+  {
+    path: '/cart',
+    name: 'Cart',
+    component: Cart,
+    meta: { requiresAuth: false }
+  },
+  {
+    path: '/profile',
+    name: 'ProfilPage',
+    component: ProfilPage,
+    meta: { requiresAuth: true }
+  },
+  {
+    path: '/search',
+    name: 'SearchResults',
+    component: SearchResult
+  },
 
-    {
-        path: '/checkout',
-        name: 'Checkout',
-        component: Checkout,
-    },/*{
+  {
+    path: '/checkout',
+    name: 'Checkout',
+    component: Checkout
+  } /*{
     path: '/admin',
     name: 'AdminLayout',
     component: AdminLayout,
@@ -117,36 +118,44 @@ const routes = [
       { path: 'orders', name: 'OrderManagement', component: OrderManagement },
       { path: 'list', name: 'ListItems', component: ListItems },
     ],
-  },*/
- 
+  },*/,
+
   {
     path: '/payment',
     name: 'payment',
-    component: Payment,
+    component: Payment
   },
   {
     path: '/paymentCheckout',
     name: 'paymentCheckout',
-    component: PaymentCommande,
-  }
-];
+    component: PaymentCommande
+  },
+  { path: '/paymentSuccess',
+    name: 'PaymentSuccess',
+    component: PaymentSuccess 
+},
+{ path: '/paymentCancel',
+    name: 'paymentCancel',
+    component: PaymentCancel 
+}
+]
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes
-});
+  history: createWebHistory(),
+  routes
+})
 
 // Garde de navigation pour vérifier l'authentification et les rôles
 router.beforeEach((to, from, next) => {
-    const authStore = useAuthStore();
+  const authStore = useAuthStore()
 
-    if (to.meta.requiresAuth && !authStore.isAuthenticated) {
-        next({ name: 'Login' });
-    } else if (to.meta.requiresAdmin && authStore.user?.role !== 'admin') {
-        next({ name: 'Home' });
-    } else {
-        next();
-    }
-});
+  if (to.meta.requiresAuth && !authStore.isAuthenticated) {
+    next({ name: 'Login' })
+  } else if (to.meta.requiresAdmin && authStore.user?.role !== 'admin') {
+    next({ name: 'Home' })
+  } else {
+    next()
+  }
+})
 
-export default router;
+export default router
