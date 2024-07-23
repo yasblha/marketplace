b2bcff43591c18f8c59b251cb7ca749fe8286f8e
zaengetsu <106625404+zaengetsu@users.noreply.github.com>
2024-07-23 17:07:48 +0200

    correction des routes post paiment

--- Backend/controllers/StripeController.js
+++ Backend/controllers/StripeController.js
@@ -4,25 +4,31 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY); // Récupère l
 exports.createCheckoutSession = async (req, res) => {
   const { items, customer } = req.body;
 
+  console.log('Received items:', items);
+  console.log('Received customer:', customer);
+
   try {
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ['card'],
       line_items: items.map(item => ({
         price_data: {
           currency: 'usd',
-          unit_amount: item.price ? item.price * 100 : 10 * 100, // Montant de test en cents ou celui envoyé
+          product_data: {
+            name: item.name || "Test Product" // Nom du produit
+          },
+          unit_amount: item.price * 100, // Montant en cents
         },
-        quantity: item.quantity || 2, // Quantité de test ou celle envoyée
+        quantity: item.quantity || 1, // Quantité envoyée ou par défaut à 1
       })),
-      customer_email: customer.email || "test@example.com", // Email de test ou celui envoyé
+      customer_email: customer.email || "test@example.com", // Email envoyé ou par défaut
       mode: 'payment',
-      success_url: 'http://localhost:8080/success',
-      cancel_url: 'http://localhost:8080/cancel',
+      success_url: 'http://localhost:5173/paymentSuccess',
+      cancel_url: 'http://localhost:5173//paymentCancel',
     });
 
     res.json({ sessionId: session.id });
   } catch (error) {
     console.error('Error creating checkout session:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).json({ error: 'Internal Server Error', details: error.message });
   }
 };
