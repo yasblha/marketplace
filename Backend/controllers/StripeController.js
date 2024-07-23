b3b1498ce4771e85f8bd09f07539e386124bae28
zaengetsu <106625404+zaengetsu@users.noreply.github.com>
2024-07-23 18:08:21 +0200

    ajout de la fonction relative au payment intent, dans le controller de Stripe

--- Backend/controllers/StripeController.js
+++ Backend/controllers/StripeController.js
@@ -1,6 +1,18 @@
 require('dotenv').config();
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY); // Récupère la clé secrète Stripe depuis .env
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
+/**
+ * Creates a Stripe Checkout Session to handle the payment process.
+ * 
+ * @param {Object} req - The request object.
+ * @param {Object} req.body - The body of the request.
+ * @param {Array} req.body.items - The items to be purchased.
+ * @param {Object} req.body.customer - The customer information.
+ * @param {string} req.body.customer.email - The customer's email address.
+ * @param {Object} res - The response object.
+ * 
+ * @returns {void}
+ */
 exports.createCheckoutSession = async (req, res) => {
   const { items, customer } = req.body;
 
@@ -23,12 +35,42 @@ exports.createCheckoutSession = async (req, res) => {
       customer_email: customer.email || "test@example.com", // Email envoyé ou par défaut
       mode: 'payment',
       success_url: 'http://localhost:5173/paymentSuccess',
-      cancel_url: 'http://localhost:5173//paymentCancel',
+      cancel_url: 'http://localhost:5173/paymentCancel',
     });
 
     res.json({ sessionId: session.id });
   } catch (error) {
     console.error('Error creating checkout session:', error);
+    res.status(500).json({ error: 'Internal Server Error', details: error.message });
+  }
+};
+
+/**
+ * Creates a Stripe Payment Intent to handle the payment process.
+ * 
+ * @param {Object} req - The request object.
+ * @param {Object} req.body - The body of the request.
+ * @param {number} req.body.amount - The total amount to be charged in cents.
+ * @param {Object} req.body.customer - The customer information.
+ * @param {string} req.body.customer.email - The customer's email address.
+ * @param {Object} res - The response object.
+ * 
+ * @returns {void}
+ */
+exports.createPaymentIntent = async (req, res) => {
+  const { amount, customer } = req.body;
+
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: amount,
+      currency: 'usd',
+      payment_method_types: ['card'],
+      receipt_email: customer.email
+    });
+
+    res.json({ clientSecret: paymentIntent.client_secret });
+  } catch (error) {
+    console.error('Error creating payment intent:', error);
     res.status(500).json({ error: 'Internal Server Error', details: error.message });
   }
 };
