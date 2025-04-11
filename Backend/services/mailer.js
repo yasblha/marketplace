e2cc6da43409520c5196faf969615b5b6621b8e5
BOULAHNINE Yassine <108906340+yasblha@users.noreply.github.com>
2025-04-11 10:29:08 +0200

    Update mailer.js

--- Backend/services/mailer.js
+++ Backend/services/mailer.js
@@ -3,14 +3,14 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: 'yassineboul98@gmail.com',
-        pass: 'syhm zjkv pjmd atak',
+        user: 'ecom@mail.com',
+        pass: 'pass',
     },
 });
 
 async function sendEmail(to, subject, htmlContent) {
     const mailOptions = {
-        from: 'yassineboul98@gmail.com',
+        from: 'ecom@mail.com',
         to,
         subject,
         html: htmlContent,
